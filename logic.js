58e4ed3cfa203ea1ccc453efa7b79464b03f65ee
Johanna-hub <jyherman@gmail.com>
2017-11-08 16:41:21 +0000

    cleanAir function v2

--- logic.js
+++ logic.js
@@ -17,19 +17,26 @@ function cleanAir(bCode) {
       for (var i = 0; i < siteArr.length; i++) {
         if (Array.isArray(siteArr[i].Species)) {
           for (var j = 0; j < siteArr[i].Species.length; j++) {
-            pollutantOne[siteArr[i].Species[j]['@SpeciesCode']].push(siteArr[i].Species[j]['@AirQualityIndex']);
+            pollutantOne[siteArr[i].Species[j]['@SpeciesCode']].push(Number(siteArr[i].Species[j]['@AirQualityIndex']));
           }
         } else {
-          pollutantOne[siteArr[i].Species['@SpeciesCode']].push(siteArr[i].Species['@AirQualityIndex']);
+          pollutantOne[siteArr[i].Species['@SpeciesCode']].push(Number(siteArr[i].Species['@AirQualityIndex']));
 
         }
       }
-      
-      var nitroLevel = pollutantOne['NO2'];
-      finalVal["NO2"] = nitroLevel.reduce(function(a, b) {
+      console.log(pollutantOne);
+      for(var key in pollutantOne){
+      if(pollutantOne[key].length > 0){
+      finalVal[key] = pollutantOne[key].reduce(function(a, b) {
         return Math.max(a, b);
-      });
-      console.log(finalVal);
+      })
+    }
+  }
+  console.log(finalVal);
+      // var nitroLevel = pollutantOne['NO2'];
+      // finalVal["NO2"] = nitroLevel.reduce(function(a, b) {
+      //   return Math.max(a, b);
+      // });
     }
   };
 
