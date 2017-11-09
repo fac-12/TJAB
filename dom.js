25f15f72af3f0429ee28da97a08233826d264707
rb50 <rebecca-holmes@live.co.uk>
2017-11-09 16:56:23 +0000

    info displaying on the page

--- dom.js
+++ dom.js
@@ -1,21 +1,22 @@
 
-
-
-
 function createListener(){
   for (var i=1; i<=33;i++){
+    console.log("EL on");
     document.getElementById(i.toString()).addEventListener('click',function(event){
+    console.log('click working');
     parallelFunction(cleanAir,updateDom,event.target.id);
   });
   }
 
 }
 
+createListener();
+
 function addInfo(obj){
   var information=document.getElementById('info');
   for (var key in obj){
     var para=document.createElement('p');
-    var text=document.createTextContent(key+':'+obj[key]);
+    var text=document.createTextNode(key+':'+obj[key]);
     para.appendChild(text);
     information.appendChild(para);
   }
@@ -24,8 +25,10 @@ function addInfo(obj){
 }
 
 function addGif(image){
+  var img = document.createElement('img');
   var giffy =document.getElementById('gif');
-  giffy.src= image;
+  img.src= image;
+  giffy.appendChild(img);
 }
 
 function addColor(num){
@@ -33,10 +36,10 @@ function addColor(num){
     if(num===1){
       ratingColor.className='green';
       }
-      else(num===2){
+      else if(num===2){
         ratingColor.className= 'orange';
       }
-      else(num===3){
+      else if(num===3){
         ratingColor.className= 'red';
       }
 }
